564861e11f9297cb144e11e81cd2fd4445475d48
Venkata R Sundaragiri <sundaragiriv@gmail.com>
2025-09-14 12:18:28 -0400

    Remove basename from BrowserRouter, rebuild for correct routing locally and online, restore CNAME

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-  <BrowserRouter basename="/venkataSite">
+  <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/techhBlogs" element={<TechhBlogs />} />
