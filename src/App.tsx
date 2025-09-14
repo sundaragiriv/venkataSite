827217c9d7af0bff8d0836875fb586a5b16b7c0c
Venkata R Sundaragiri <sundaragiriv@gmail.com>
2025-09-14 11:09:47 -0400

    Set BrowserRouter basename for GitHub Pages deployment to /venkataSite

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+  <BrowserRouter basename="/venkataSite">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/techhBlogs" element={<TechhBlogs />} />
